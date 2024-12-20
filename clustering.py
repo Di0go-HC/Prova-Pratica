031b1653705cdf7bbcabe67e45130e7e1e9d2eb4
Di0go-HC <168016929+Di0go-HC@users.noreply.github.com>
2024-12-20 20:04:09 -0300

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -4,15 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-# Exemplo de dados hipotéticos extraídos do seu dataset
-# Aqui estamos usando apenas colunas numéricas para simplificação
 data = {
     'Transacao': [376.24, 489.56, 200.12, 600.45, 122.33, 550.23, 300.45, 430.90],
     'Valor': [141.00, 230.00, 120.00, 300.00, 150.00, 200.00, 100.00, 250.00],
     'ID_Cliente': [13758.91, 13759.00, 13760.23, 13761.12, 13762.54, 13763.33, 13764.23, 13765.12]
 }
 
-# Convertendo para DataFrame
 df = pd.DataFrame(data)
 
 # Normalizando os dados para garantir que todas as variáveis tenham o mesmo peso
@@ -34,7 +31,7 @@ plt.xlabel('Número de Clusters')
 plt.ylabel('Inércia')
 plt.show()
 
-# A partir do gráfico de cotovelo, podemos definir o número de clusters ideal
+# A partir do gráfico de cotovelo, pode-se definir o número de clusters ideal
 # Agora, vamos aplicar o K-Means com o número de clusters escolhido (suponhamos K=3)
 kmeans = KMeans(n_clusters=3, random_state=23)
 kmeans.fit(X_scaled)
